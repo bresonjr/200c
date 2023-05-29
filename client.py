409d85f7972bcafa78d9335448b45ceaef5e68e7
bresonjr <79446230+bresonjr@users.noreply.github.com>
2023-05-29 15:21:26 +0530

    client.py

--- client.py
+++ client.py
@@ -1,34 +1,64 @@
 import socket
-import threading
+from threading import Thread
+from tkinter import *
 
-def receive(client_socket):
-    while True:
-        try:
-            message = client_socket.recv(1024).decode('utf-8')
-            if message == 'NICKNAME':
-                client_socket.send(nickname.encode('utf-8'))
-            else:
-                print(message)
-        except Exception as e:
-            print("An error occurred:", str(e))
-            client_socket.close()
-            break
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def write(client_socket):
-    while True:
-        message = input()
-        client_socket.send(message.encode('utf-8'))
+ip_address = '127.0.0.1'
+port = 8000
 
-nickname = input("Enter your nickname: ")
+client.connect((ip_address, port))
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_ip = '127.0.0.1'  # Replace with the actual IP address of the server
-server_port = 12345  # Replace with the actual port number on which the server is running
+print("Connected with the server...")
 
-client_socket.connect((server_ip, server_port))
 
-receive_thread = threading.Thread(target=receive, args=(client_socket,))
-write_thread = threading.Thread(target=write, args=(client_socket,))
+class GUI:
+    def __init__(self):
+        self.Window = Tk()
+        self.Window.withdraw()
+        self.login = Toplevel()
+        self.login.title("Login")
+        self.login.resizable(width=False, height=False)
+        self.login.configure(width=400, height=300)
+        self.pls = Label(
+            self.login,
+            text="Please login to continue",
+            justify=CENTER,
+            font="Helvetica 14 bold"
+        )
+        self.pls.place(relheight=0.15, relx=0.2, rely=0.07)
+        self.labelname = Label(self.login, text="Name: ", font="Helvetica 12")
+        self.labelname.place(relheight=0.2, relx=0.1, rely=0.2)
+        self.entryname = Entry(self.login, font="Helvetica 14")
+        self.entryname.place(relwidth=0.4, relheight=0.12, relx=0.35, rely=0.2)
+        self.entryname.focus()
+        self.go = Button(
+            self.login,
+            text="CONTINUE",
+            font="Helvetica 14 bold",
+            command=lambda: self.goAhead(self.entryname.get())
+        )
+        self.go.place(relx=0.4, rely=0.55)
+        self.Window.mainloop()
 
-receive_thread.start()
-write_thread.start()
+    def goAhead(self, name):
+        self.login.destroy()
+        self.name = name
+        recv = Thread(target=self.receive)
+        recv.start()
+
+    def receive(self):
+        while True:
+            try:
+                message = client.recv(2048).decode('utf-8')
+                if message == 'NICKNAME':
+                    client.send(self.name.encode('utf-8'))
+                else:
+                    pass
+            except:
+                print("An error occurred!")
+                client.close()
+                break
+
+
+g = GUI()
