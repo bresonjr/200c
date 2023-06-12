b4ff37b7f0851db489a2b1bf2b1a09690a730a7c
bresonjr <79446230+bresonjr@users.noreply.github.com>
2023-06-12 21:34:21 +0530

    Updatedserver.py

--- server.py
+++ server.py
@@ -1,29 +1,38 @@
 import socket
 import threading
 
-def clientthread(client_socket, client_address):
-    nickname = client_socket.recv(1024).decode('utf-8')
-    nicknames.append(nickname)
-
+def handle_client(client_socket, client_address):
     while True:
-        # Your server logic here
-        pass
+        try:
+            data = client_socket.recv(2048)
+            if data:
+                message = data.decode('utf-8')
+                print(f"Received message from {client_address}: {message}")
+                # Handle the received message or perform server logic here
+            else:
+                print(f"Connection closed by {client_address}")
+                break
+        except Exception as e:
+            print(f"An error occurred with {client_address}: {str(e)}")
+            break
 
     client_socket.close()
-    nicknames.remove(nickname)
 
-nicknames = []
+def start_server(ip_address, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((ip_address, port))
+    server_socket.listen(5)
+    print(f"Server started on {ip_address}:{port}")
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_ip = '127.0.0.1'  # Replace with the actual IP address of the server
-server_port = 12345  # Replace with the actual port number on which the server will listen
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"New connection from {client_address}")
 
-server_socket.bind((server_ip, server_port))
-server_socket.listen(5)
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        client_thread.start()
 
-print("Server started on {}:{}".format(server_ip, server_port))
+# Replace with your desired IP address and port number
+ip_address = '127.0.0.1'
+port = 8000
 
-while True:
-    client_socket, client_address = server_socket.accept()
-    client_thread = threading.Thread(target=clientthread, args=(client_socket, client_address))
-    client_thread.start()
+start_server(ip_address, port)
